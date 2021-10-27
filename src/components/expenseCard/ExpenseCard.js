7663357ecac233a2421adeb36f330258ab5e246a
Vishwas-MH <91179257+Vishwas-MH@users.noreply.github.com>
2021-10-27 12:41:33 +0530

    media queries

--- src/components/expenseCard/ExpenseCard.js
+++ src/components/expenseCard/ExpenseCard.js
@@ -4,31 +4,31 @@ import icon_approve from "../../assets/images/dashboard/icon_approve.svg";
 import "./ExpenseCard.css"
 
 const ExpenseCard = (props) => {
-    return(
+    return (
         <>
-        <div className="expense-card-left">
-            <div className="emp-img-id">
-                <img className="emp-img" src={props.icon} alt=""></img>
-                <div className="emp-id-name">
-                    <span className="emp-id">{props.emp}</span>
-                    <span className="emp-name">{props.name}</span>
+            <div className="expense-card-left">
+                <div className="emp-img-id">
+                    <img className="emp-img" src={props.icon} alt=""></img>
+                    <div className="emp-id-name">
+                        <span className="emp-id">{props.emp}</span>
+                        <span className="emp-name">{props.name}</span>
+                    </div>
+                </div>
+                <div className="expense-details">
+                    <span className="expense-dates">Dates: {props.dates}</span>
+                    <span className="expense-comment">Comments: {props.comments}</span>
                 </div>
             </div>
-            <div className="expense-details">
-                <span className="expense-dates">Dates: {props.dates}</span>
-                <span className="expense-comment">Comments: {props.comments}</span>
+            <div className="expense-card-right">
+                <button className="reject-button">
+                    <img className="reject-img" src={icon_reject} alt=""></img>
+                    <span className="reject-text">Reject</span>
+                </button>
+                <button className="approve-button">
+                    <img className="approve-img" src={icon_approve} alt=""></img>
+                    <span className="approve-text">Approve</span>
+                </button>
             </div>
-        </div>
-        <div className="expense-card-right">
-            <button className="reject-button">
-                <img className="reject-img" src={icon_reject} alt=""></img>
-                <span className="reject-text">Reject</span>
-            </button>
-            <button className="approve-button">
-            <img className="approve-img" src={icon_approve} alt=""></img>
-                <span className="approve-text">Approve</span>
-            </button>
-        </div>
         </>
     )
 };
