b6e6d1ec4aeb2e8469a6c835d69783c093fc27df
Vishwas-MH <91179257+Vishwas-MH@users.noreply.github.com>
2021-10-27 14:55:43 +0530

    gitignore

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import Header from './components/header/Header';
 import Navbar from './components/navbar/Navbar';
 import Dashboard from './pages/dashboard/Dashboard';
+import Employees from './pages/employees/Employees';
 
 const App = () => {
   return (
-    <div className="App">
-      <Header />
-      <main>
-        <Navbar />
-        <Dashboard />
-      </main>
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <main>
+          <Navbar />
+          <Switch>
+            <Route exact path = "/">
+              <Redirect to="/Dashboard"/>
+            </Route>
+          </Switch>
+          <Switch>
+            <Route exact path = "/Dashboard" component={Dashboard}></Route>
+          </Switch>
+          <Switch>
+            <Route exact path = "/Employees" component={Employees}></Route>
+          </Switch>
+        </main>
+      </div>
+    </Router>
   );
 }
 
--- src/pages/employees/Employees.js
+++ src/pages/employees/Employees.js
@@ -2,7 +2,9 @@ import React from "react";
 import "./Employees.css";
 
 const Employees = () => {
-
+    return(
+        <h2>Employees</h2>
+    )
 }
 
 export default Employees;
